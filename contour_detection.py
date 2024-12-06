2ca78df2981c7654ba14143f005e8fd96a655ef6
JEESEUNGCHAN <jeesz11@gachon.ac.kr>
2024-12-06 15:48:49 +0900

    contour_detection.py

--- contour_detection.py
+++ contour_detection.py
@@ -2,11 +2,22 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-# read image
+# Read image
 image = cv2.imread("./images/newyork.jpg")
 
-# convert image to gray image
+# Convert image to gray image
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# binarization process
+# Binarization process
 _, thresh = cv2.threshold(gray_image, 100, 255, cv2.THRESH_BINARY)
+
+# Draw contours on the original image
+contour_image = image.copy()
+cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 3)
+
+# Out contour image
+plt.imshow(cv2.cvtColor(contour_image, cv2.COLOR_BGR2RGB))
+plt.title("Contours Detected")
+plt.axis("off")
+plt.show()
+
